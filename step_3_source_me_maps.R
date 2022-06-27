4a4ef006bf2660da3112ce060cbd63f47b8f47f9
rich <rich.p.martin@gmail.com>
2022-06-27 06:37:50 -0700

    changed map

--- step_3_source_me_maps.R
+++ step_3_source_me_maps.R
@@ -15,8 +15,8 @@ if(!"tidyverse" %in% names(sessionInfo()$otherPkgs)) library(tidyverse)
 #functions-----------
 create_map <- function(df, var, facet=TRUE){
   plt <-  ggplot(data=df)+
-    geom_sf(data=bc_map, colour="grey80")+
-    geom_sf(mapping=aes(fill={{  var  }}))+
+    geom_sf(data=bc_map, lwd=0)+
+    geom_sf(mapping=aes(fill={{  var  }}), lwd=.2, colour="white")+
     scale_fill_viridis_c(trans="log10", label=scales::comma)+
     theme_void()
   if(facet==TRUE){
@@ -26,14 +26,7 @@ create_map <- function(df, var, facet=TRUE){
   return(plt)
 }
 #read in the data---------
-bc_map <- sf::st_read(here::here("map_data","CNCNMCRGN1_polygon.shp"))%>%
-  janitor::clean_names()%>%
-  separate(cnmcrgnnm, into=c("region","french"), sep= "/")%>%
-  select(region, geometry)%>%
-  mutate(region=janitor::make_clean_names(region),
-         region=str_replace_all(region,"_and_","_"),
-         region=str_remove_all(region,"lower_"))%>%
-  arrange(region)
+bc_map <- read_rds(here::here("map_data","bc_region_sf.rds"))
 
 last_quarter <- readRDS(here::here("processed_data","mpi_shortraw.rds"))%>%
   filter(quarter==max(quarter))%>%
@@ -57,10 +50,11 @@ last_quarter_disaggregated <- last_quarter%>%
 disaggregated <- left_join(last_quarter_disaggregated, bc_map, by="region")%>%
   sf::st_as_sf()
 aggregated <- left_join(last_quarter_aggregated, bc_map, by="region")%>%
-  sf::st_as_sf()
+  sf::st_as_sf()%>%
+  mutate(id=row_number())
 
 by_region_map <- create_map(aggregated, `Total Project Cost (M)`, facet=FALSE)+
-  ggsflabel::geom_sf_label_repel(aes(label = paste(str_to_title(str_replace_all(region,"_"," ")), scales::dollar(`Total Project Cost (M)`, suffix=" (M)"), sep="\n")))
+  ggsflabel::geom_sf_label_repel(data=aggregated[-c(6,7,8,9,10,13),], aes(label = paste(str_to_title(str_replace_all(region,"_"," ")), scales::dollar(`Total Project Cost (M)`, suffix=" (M)"), sep="\n")))
 by_region_type_and_stage_map <- create_map(disaggregated, `Total Project Cost (M)`)
 
 ggsave(here::here("processed_data", "by_region_map.png"),
